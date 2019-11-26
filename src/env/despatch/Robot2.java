740568e7024afa468efe162f0d592ac3695ee608
root <root@nb-20775.Unisg.ch>
2019-11-26 13:23:44 +0100

    demo v.-1

---
+++ src/env/despatch/Robot2.java
@@ -0,0 +1,32 @@
+package despatch;
+
+import cartago.*;
+
+public class Robot2 extends Artifact {
+	void init(Object coordinates) {
+		defineObsProperty("location", coordinates);
+	}
+
+	@OPERATION
+	void initialize(String mode){
+		signal("initialized", mode);
+	}
+
+	@OPERATION
+	void move(Object[] coordinates){
+		ObsProperty location = getObsProperty("location");
+		location.updateValue(coordinates);
+		signal("moved", location.getValues());
+	}
+
+	@OPERATION
+	void grasp(){
+		signal("grasped");
+	}
+
+	@OPERATION
+	void release(){
+		signal("released");
+	}
+
+}
