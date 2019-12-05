1a465fdd8f424fe59e06aab7fb71066f4fa5a14e
danaivacht <ece7999@upnet.gr>
2019-12-05 15:21:06 +0100

    move robot1

---
+++ jacamo-app/src/env/www/Robot1.java
@@ -0,0 +1,28 @@
+package www;
+
+import java.util.*;
+import cartago.*;
+
+public class Robot1 extends Artifact {
+	void init(Object coordinates) {
+		defineObsProperty("location", coordinates);
+	}
+
+	@OPERATION
+	void move(Object[] coordinates){
+		ObsProperty location = getObsProperty("location");
+		location.updateValue(coordinates);
+		signal("moved", location.getValues());
+	}
+
+	@OPERATION
+	void grasp(){
+		signal("grasped");
+	}
+
+	@OPERATION
+	void release(){
+		signal("released");
+
+	}
+}
