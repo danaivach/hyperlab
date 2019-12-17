fc0d86a23df4f49ff29bd1f88e2d32c3fddf1f64
phil9987 <philipjunker@gmail.com>
2019-12-17 11:31:36 +0100

    separate payload from setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,11 +5,7 @@ curl -i -X POST \
   http://localhost:8080/environments/ \
   -H 'content-type: text/turtle' \
   -H 'slug: shopfloor' \
-  --data-raw '@prefix eve: <http://w3id.org/eve#> .
-
-              <>
-                a eve:Environment ;
-                eve:contains <http://localhost:8080/workspaces/interactionsWksp> .'
+  -d '@payload/0_env_payload.ttl'
 
 sleep 1
 
@@ -18,36 +14,18 @@ curl -i -X POST \
   http://localhost:8080/workspaces/ \
   -H 'content-type: text/turtle' \
   -H 'slug: interactionsWksp' \
-  --data-raw '@prefix eve: <http://w3id.org/eve#> .
-
-              <>
-                a eve:Workspace;
-                eve:hasName "interactionsWksp";
-                eve:contains <http://localhost:8080/artifacts/robot1>;
-                eve:contains <http://localhost:8080/artifacts/robot2>.'
+  -d '@payload/1_wksp_payload.ttl'
 
 echo -e '\n\nCreating robot1 artifact...'
 curl -i -X POST \
   http://localhost:8080/artifacts/ \
   -H 'content-type: text/turtle' \
   -H 'slug: robot1' \
-  --data-raw '@prefix eve: <http://w3id.org/eve#> .
-
-              <>
-                a eve:Artifact ;
-                eve:hasName "robot1" ;
-                eve:isRobot "robot1" ;
-                eve:hasCartagoArtifact "www.Robot1" .'
+  -d '@payload/2_robot1_payload.ttl'
 
 echo -e '\n\nCreating robot2 artifact...'
 curl -i -X POST \
   http://localhost:8080/artifacts/ \
   -H 'content-type: text/turtle' \
   -H 'slug: robot2' \
-  --data-raw '@prefix eve: <http://w3id.org/eve#> .
-
-              <>
-                a eve:Artifact ;
-                eve:hasName "robot2" ;
-                eve:isRobot "robot2" ;
-                eve:hasCartagoArtifact "www.Robot2" .'
+  -d '@payload/3_robot2_payload.ttl'
