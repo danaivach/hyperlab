3741e0e018890d5746d0b66c20c88d6658aa123d
Philip Junker <philipjunker@gmail.com>
2019-12-19 17:57:41 +0100

    receive placeObject message

--- jacamo-app/src/env/www/ShopFloorMap.java
+++ jacamo-app/src/env/www/ShopFloorMap.java
@@ -11,6 +11,8 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 public class ShopFloorMap extends Artifact{
 
@@ -22,44 +24,33 @@ public class ShopFloorMap extends Artifact{
 		// add listener
 		this.ws.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
 			public void handleMessage(String message) {
-				System.out.println(message);
+				System.out.println("Websocket received: " + message);
+				try {
+					JSONObject jo = new JSONObject(message);
+					if (jo.has("jacamo")) {
+						jo = (JSONObject) jo.get("jacamo");
+						if (jo.has("placeObject")) {
+							jo = (JSONObject) jo.get("placeObject");
+							int x = jo.getInt("x");
+							int y = jo.getInt("y");
+							System.out.println("received request to placeObject at x: " + String.valueOf(x) + " / y:" + String.valueOf(y));
+						}
+					}
+				} catch(JSONException e) {
+					System.out.println("JSONException: failed to parse JSON message");
+				}
 			}
 		});
-		ws.sendMessage("{'event':'addChannel','channel':'ok_btccny_ticker'}");
 
 	}
 
 	@OPERATION
 	void rotate(String artifactName, int degrees){
-		HttpPost request = new HttpPost(UIAddress + "/" + artifactName + "/rotate");
-		try{
-			String payload = "{\n" +
-				" \"degrees\": " + degrees +"\n" +
-			"}";
-			request.setEntity(new StringEntity(payload));
-			request.addHeader("content-type", "application/json");
-			HttpClient client = HttpClientBuilder.create().build();
-			HttpResponse response = client.execute(request);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+		this.ws.sendMessage("{\"" + artifactName + "\":{\"rotate\":{\"degrees\":" + degrees + "}}}");
 	}
 
 	@OPERATION
 	void move(String artifactName,int x, int y){
-		HttpPost request = new HttpPost(UIAddress + "/" + artifactName + "/move");
-		try {
-			String payload = "{\n" +
-				" \"x\": " + x + ",\n" +
-				" \"y\": " + y + "\n" +
-			"}";
-			request.setEntity(new StringEntity(payload));
-			request.addHeader("content-type", "application/json");
-			HttpClient client = HttpClientBuilder.create().build();
-			HttpResponse response = client.execute(request);
-			//System.out.println("Response: " + response.getStatusLine().getStatusCode());
-		} catch (Exception e){
-			e.printStackTrace();
-		}
+		this.ws.sendMessage("{\"" + artifactName + "\":{\"move\":{\"x\":" + x + ", \"y\":" + y + "}}}");
 	}
 }
