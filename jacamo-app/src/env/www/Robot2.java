e0eabc736310a690ceb32fe45265e042cbe04bae
danaivacht <ece7999@upnet.gr>
2020-01-15 12:26:21 +0100

    remove init

--- jacamo-app/src/env/www/Robot2.java
+++ jacamo-app/src/env/www/Robot2.java
@@ -4,12 +4,6 @@ import java.util.concurrent.TimeUnit;
 
 public class Robot2 extends Artifact {
 	
-	void init(){
-
-	defineObsProperty("hasProtocol","drive(X1,Y1,X2,Y2)", "true" , 
-		"+!drive(X1,Y1,X2,Y2) : true <- move(X1,Y1); load; move(X2,Y2); unload. ");
-	}
-
 	@OPERATION
 	void move(int x,int y){
 		signal("moving",x,y);
